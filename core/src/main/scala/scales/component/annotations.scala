a82bb3c47404b67e61c026361702eba16a5c0d94
Xavier Cho <mysticfallband@gmail.com>
2015-08-06 11:41:33 +0900

    Fix #2: Support @attribute annotation to inject attributes

--- core/src/main/scala/scales/component/annotations.scala
+++ core/src/main/scala/scales/component/annotations.scala
@@ -14,4 +14,17 @@ class tag(name: String) extends ComponentAnnotation
 
 class prototype(name: String) extends ComponentAnnotation
 
+class attribute extends PropertyAnnotation {
+
+  private var _name: Option[String] = None
+
+  def this(name: String) {
+    this()
+
+    this._name = Some(name)
+  }
+
+  def name: Option[String] = _name
+}
+
 class enumerable extends PropertyAnnotation
--- core/src/main/scala/scales/di/internal/ServiceFactoryImpl.scala
+++ core/src/main/scala/scales/di/internal/ServiceFactoryImpl.scala
@@ -1,22 +1,54 @@
 package scales.di.internal
 
-import scala.language.experimental.macros
 import scala.reflect.macros.blackbox.Context
 
+import scales.component.attribute
 import scales.di.Scope
+import scales.macros.AnnotationUtils
 
 object ServiceFactoryImpl {
+
+  import AnnotationUtils._
 
   def newInstance[A](c: Context)(scope: c.Expr[Scope])(implicit tag: c.WeakTypeTag[A]): c.Expr[A] = {
     import c.universe._
 
     val tpe = tag.tpe
 
-    val overrides: List[DefDef] = Nil
-
     c.Expr[A] {
       if (tpe.typeSymbol.isAbstract) {
         val name = TypeName(tpe.typeSymbol.name.decodedName.toString + "Impl")
+
+        def isValid(method: MethodSymbol) =
+          method.isPublic &&
+            method.paramLists.isEmpty &&
+            hasMemberAnnotation[attribute](c)(method)
+
+        def find(annotatedOnly: Boolean): PartialFunction[Symbol, Tree] = {
+          case m: MethodSymbol if isValid(m) =>
+            val name = getMemberAnnotation[attribute](c)(m).map(TermName(_)) getOrElse m.name
+            val attr = Literal(Constant(name.decodedName.toString))
+
+            val rt = m.returnType
+
+            if (rt.typeSymbol.fullName == "scala.Option") {
+              val t = rt.typeArgs.head
+
+              q"""override def ${m.name}: Option[$t] = Option(element.getAttribute($attr)).map(_.asInstanceOf[$t])"""
+            } else {
+              if (m.isAbstract) {
+                q"""override def ${m.name}: $rt = element.getAttribute($attr).asInstanceOf[$rt]"""
+              } else {
+                q"""
+                  override def ${m.name}: $rt = Option(element.getAttribute($attr)).map(_.asInstanceOf[$rt]) getOrElse {
+                    super.${m.name}
+                  }
+                """
+              }
+            }
+        }
+
+        val overrides = tag.tpe.members collect find(true)
 
         q"""
           class $name extends $tpe {
